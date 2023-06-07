0b68be40424b7f82da25d7da34847dedb1bf797b
alexx73 <tristanluna@yahoo.com.ar>
2023-06-07 14:53:05 -0300

    actividad b agregada

---
+++ DateTime - actividad b/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_B
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            DateTime inicio = DateTime.Now; // Obtener el momento de inicio
+
+            for (int i = 0; i < 100; i++) // código que se mide
+            {
+                Console.Write("{0} ", i + 1);
+            }
+            Console.WriteLine();
+
+            DateTime fin = DateTime.Now; // fin del contador de tiempo
+
+            TimeSpan tiempoTranscurrido = fin - inicio;
+            double tiempoEnMilisegundos = tiempoTranscurrido.TotalMilliseconds;
+
+            Console.WriteLine("Tiempo de ejecución: " + tiempoEnMilisegundos + " milisegundos");
+            Console.WriteLine("(el tiempo depende de la velocidad del ordenador)");
+            Console.ReadLine();
+        }
+    }
+}
+
